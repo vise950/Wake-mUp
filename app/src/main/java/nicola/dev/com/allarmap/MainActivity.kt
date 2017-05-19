09b6a5afc793b03e88895061dce6c3c9b16349a3
Unknown <nicoladev95@gmail.com>
2017-05-19 22:52:04 +0200

    add map

--- app/src/main/java/nicola/dev/com/allarmap/MainActivity.kt
+++ app/src/main/java/nicola/dev/com/allarmap/MainActivity.kt
@@ -2,11 +2,43 @@ package nicola.dev.com.allarmap
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),OnMapReadyCallback {
+
+    private var map: GoogleMap? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initializeMap()
+    }
+
+    //todo add permission request
+
+    override fun onMapReady(map: GoogleMap?) {
+        map?.let {
+            this.map = it
+            setMapUi(it)
+        }
+    }
+
+    private fun initializeMap() {
+        if (map == null) {
+            val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this)
+        }
+    }
+
+    private fun setMapUi(map: GoogleMap) {
+        map.isMyLocationEnabled = true
+        map.uiSettings.isMyLocationButtonEnabled = true
+        map.uiSettings.isRotateGesturesEnabled = false
     }
 }
+
+
+//todo capire come funzionano le key sulla google console e i sha1 delle app
